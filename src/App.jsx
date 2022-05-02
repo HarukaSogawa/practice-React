07295338143f72e1c063ea968d7f91b07c3e52b3
h-sogawa <89685474+h-sogawa@users.noreply.github.com>
2022-05-02 12:54:01 +0900

    useState stateと再レンダリングについて。

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,34 @@ import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("start");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  if (num > 0) {
+    if (num % 3 === 0) {
+      faceShowFlag || setFaceShowFlag(true);
+    } else {
+      faceShowFlag && setFaceShowFlag(false);
+    }
+  }
+
   return (
     <>
       <h1 style={{ color: "red" }}>Hello!</h1>
       <ColorfulMessage color="blue">How are you?</ColorfulMessage>
       <ColorfulMessage color="pink">I am fine!</ColorfulMessage>
       <button onClick={onClickCountUp}>Count Up</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>＼(^o^)／</p>}
     </>
   );
 };
